4426f6b41e7ea7ab614775f6722d8cf63149f8c4
Nícolas Hecker Silva <nicolashecker.nhs@gmail.com>
2025-10-11 17:40:07 -0300

    inicio de raylib, morre java

--- src/engine/AbstractGame.java
+++
@@ -1,7 +0,0 @@
-package engine;
-
-public interface AbstractGame
-{
-	public void update();
-    public void renderer(Renderer r);
-}
--- src/game/MenuStates.java
+++
@@ -1,6 +0,0 @@
-package game;
-
-public enum MenuStates {
-
-	MAIN, GAME;
-}
